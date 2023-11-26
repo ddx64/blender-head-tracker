7cc7905633f5550c1963c57bfc4796fa953b8630
ddx64 <102793027+ddx64@users.noreply.github.com>
2023-11-26 07:41:11 -0500

    refract rotate logic

--- blender_head_tracker.py
+++ blender_head_tracker.py
@@ -8,7 +8,7 @@ bl_info = {
     "name": "Gaze Navi",
     "description": "OpenCV based camera navigator",
     "author": "ddx64",
-    "version": (1, 0),
+    "version": (1, 1),
     "blender": (3, 6, 2),
     "location": "View3D > Panel > Tool",
     "doc_url": "https://github.com/ddx64/blender-head-tracker.git",
@@ -27,7 +27,7 @@ class GAZE_OT_Zoom(bpy.types.Operator):
     def __init__(self) -> None:
         global snapshot
         self.frame_generator = snapshot()
-        self.buffer = None
+        self.buffer = (None, None)
 
     @staticmethod
     def face_filter(frame):
@@ -54,7 +54,7 @@ class GAZE_OT_Zoom(bpy.types.Operator):
             return {"FINISHED"}
 
         fsize, cnt = 0, 0
-        for _ in range(5):
+        for _ in range(3):
             k = GAZE_OT_Zoom.face_filter(next(self.frame_generator))
             if k is not None:
                 fsize += k
@@ -76,6 +76,7 @@ class GAZE_OT_Zoom(bpy.types.Operator):
 
     def invoke(self, context, event):
         self.rv3d = None
+        self.buffer = None
 
         for area in context.screen.areas:
             if area.type == "VIEW_3D":
@@ -92,30 +93,26 @@ class GAZE_OT_Rotate(bpy.types.Operator):
     bl_idname = "gaze.rotate"
     bl_label = "Gaze Navi Rotate"
 
-    buffer_size = 5
-
     def __init__(self) -> None:
         global snapshot
         self.frame_generator = snapshot()
-        self.buffer_x = []
-        self.buffer_y = []
-        self.last_offset = (0, 0)
-
-    # input -> <frame>
-    # output <- [<subframe>,(xywh)]*2
+        self.buffer = (None, None)
 
     @staticmethod
-    def eyes_detect(frame):
+    def eye_filter(frame) -> tuple:
         global face_classifier, eye_classifier
+        left, right = None, None
+
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
         face = face_classifier.detectMultiScale(
             gray, 1.1, 5, minSize=(50, 70))
-        if len(face) != 1:
-            return None, None
-        eyes = eye_classifier.detectMultiScale(gray, 1.2, 10)
 
-        left, right = None, None
+        if len(face) != 1:
+            return left, right
+
         fx, fy, fw, fh = face[0]
+
+        eyes = eye_classifier.detectMultiScale(gray, 1.2, 10)
         for x, y, w, h in eyes:
             if x < fx or x+w > fx+fw or y < fy or y+h > fy+fh:
                 continue
@@ -125,9 +122,6 @@ class GAZE_OT_Rotate(bpy.types.Operator):
                 right = (gray[y:y+h, x:x+h], (x, y, w, h))
         return left, right
 
-    # input -> <subframe>
-    # output <- (xywh)*2
-
     @staticmethod
     def pupil_filter(frame):
         pupil = cv2.HoughCircles(
@@ -136,98 +130,93 @@ class GAZE_OT_Rotate(bpy.types.Operator):
             return None
         return np.round(pupil[0, 0, :2]).astype("int")
 
-    # input -> (xywh)*4
-    # output <- (xywh)*2
-
     @staticmethod
-    def mux(left, right, left_pupil, right_pupil) -> tuple:
+    def mux(left_eye, right_eye, left_pupil, right_pupil):
+        left_ratio, right_ratio = 0, 0
         if left_pupil is None:
-            return (right_pupil[0]/right[2], right_pupil[1]/right[3])
+            right_ratio = (right_pupil[0]/right_eye[2]*100,
+                           right_pupil[1]/right_eye[3]*100)
+            return (round(right_ratio[0]), round(right_ratio[1]))
         if right_pupil is None:
-            return (left_pupil[0]/left[2], left_pupil[1]/left[3])
-        return (right_pupil[0]+left_pupil[0])/(right[2]+left[2]), \
-            (right_pupil[1]+right_pupil[1])/(right[3]+left[3])
-
-    # return changes compare to last offset
-
-    def compare(self, x, y):
-        if x is None or y is None:
-            return (0, 0)
-        sx, sy = self.last_offset
-        res = (x-sx, y-sy)
-        self.lastoffset = (x, y)
-        return res
-
-    # calaulate pupil offset, based on (50%,50%)
-    def pupil_offset(self, arg_x, arg_y) -> tuple:
-        factor_x, factor_y = 16, 9
-        if len(self.buffer_x) >= GAZE_OT_Rotate.buffer_size:
-            del self.buffer_x[0]
-            del self.buffer_y[0]
-        self.buffer_x.append(arg_x)
-        self.buffer_y.append(arg_y)
-        x = sum(self.buffer_x)/GAZE_OT_Rotate.buffer_size
-        y = sum(self.buffer_y)/GAZE_OT_Rotate.buffer_size
-        return np.round((x-0.5)*factor_x).astype('int'), \
-            np.round((y-0.5)*factor_y).astype('int')
-
-    # output final offset of one frame
-    def scan(self):
-        frame = next(self.frame_generator)
-        left, right = GAZE_OT_Rotate.eyes_detect(frame)
-        left_args, right_args = None, None
-        left_pupil, right_pupil = None, None
-        if left:
-            left_eye, left_args = left
-            left_pupil = GAZE_OT_Rotate.pupil_filter(left_eye)
-        if right:
-            right_eye, right_args = right
-            right_pupil = GAZE_OT_Rotate.pupil_filter(right_eye)
-        if left_pupil is None and right_pupil is None:
-            return None, None
-        mx, my = GAZE_OT_Rotate.mux(
-            left_args, right_args, left_pupil, right_pupil)
-        ox, oy = self.pupil_offset(mx, my)
-        # print(f"offset {ox} {oy}")
-        return ox, oy
+            left_ratio = (left_pupil[0]/left_eye[2],
+                          left_pupil[1]/left_eye[3])
+            return (round(left_ratio[0]), round(left_ratio[1]))
+        right_ratio = (right_pupil[0]/right_eye[2]*100,
+                       right_pupil[1]/right_eye[3]*100)
+        left_ratio = (left_pupil[0]/left_eye[2],
+                      left_pupil[1]/left_eye[3])
+        res = ((left_ratio[0]+right_ratio[0])*100/2,
+               (left_ratio[1]+right_ratio[1])*100/2,)
+        return (round(res[0]), round(res[1]))
 
     @classmethod
     def poll(cls, context):
         return context.scene.RNAswitch
-
-    # ignore this
 
     def execute(self, context):
         print("gaze rotate executed")
         return {'FINISHED'}
 
     def modal(self, context, event):
-        # print(event.type)
         if event.type != 'R' and 'MOUSE' not in event.type:
+            self.buffer = (None, None)
             return {"FINISHED"}
-        e_fact = context.scene.RNAefact
-        ox, oy = self.scan()
-        x, y = self.compare(ox, oy)
+
+        fratio = np.zeros(2, dtype='int')
+        cnt = 0
+        for _ in range(2):
+            left_image, right_image = None, None
+            left_args, right_args = None, None
+            left_pupil, right_pupil = None, None
+            frame = next(self.frame_generator)
+
+            left, right = GAZE_OT_Rotate.eye_filter(frame)
+            if left:
+                left_image, left_args = left
+                left_pupil = GAZE_OT_Rotate.pupil_filter(left_image)
+            if right:
+                right_image, right_args = right
+                right_pupil = GAZE_OT_Rotate.pupil_filter(right_image)
+
+            if left_pupil is None and right_pupil is None:
+                continue
+            if left_args is None and right_args is None:
+                continue
+            fx, fy = GAZE_OT_Rotate.mux(
+                left_args, right_args, left_pupil, right_pupil)
+            if fx > 100:
+                continue
+            fratio += (fx, fy)
+            cnt += 1
+
+        if cnt == 0:
+            return {"RUNNING_MODAL"}
+
+        x, y = (round(fratio[0]/cnt), round(fratio[1]/cnt))
+
+        if self.buffer == (None, None):
+            print(f"init {x} {y}")
+            self.buffer = (x, y)
+            return {"RUNNING_MODAL"}
+
         ## Rotate Viewport ##
         if self.rv3d is not None:
-            # self.rv3d.view_location.x += 1.0
-            # self.rv3d.view_location.y += 1.0
-            if x > 0:
+            print(f"output {x} {y} {self.buffer}")
+            if x > self.buffer[0]:
                 bpy.ops.view3d.view_orbit(
                     context.scene.RNAefact, type="ORBITRIGHT")
-            elif x < 0:
+            elif x < self.buffer[0]:
                 bpy.ops.view3d.view_orbit(
                     -context.scene.RNAefact, type="ORBITLEFT")
-            # bpy.ops.view3d.view_pan(self.rv3d, type="PANUP")
         ## Rotate Viewport ##
         return {"RUNNING_MODAL"}
 
     def invoke(self, context, event):
         self.rv3d = None
+        self.buffer = (None, None)
 
         for area in context.screen.areas:
             if area.type == "VIEW_3D":
-                # self.rv3d = area.spaces[0].region_3d
                 self.rv3d = {'area': area, 'region': area.regions[-1]}
 
         context.window_manager.modal_handler_add(self)
@@ -359,6 +348,7 @@ def unregister():
     ## Remove KeyMap ##
 
 # ----------UnMount Plugin----------
+
 
 # ----------script test only---------
 # if __name__ == "__main__":
